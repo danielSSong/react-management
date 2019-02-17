dfc07a95ba3c31862a09453f1aa96dad3c7c3643
danielSSong <danielzzang5517@gmail.com>
2019-02-18 02:33:06 +1100

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -18,36 +18,26 @@ const styles = theme => ({
   table: {
     minWidth: 1080
   }
-})
+});
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': 'Daniel Song',
-  'birthday': '910111',
-  'gender': 'Male',
-  'job': 'Student'
-
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': 'Lionel Messi',
-  'birthday': '123456',
-  'gender': 'Male',
-  'job': 'Student'
-
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': 'Good Job',
-  'birthday': '910111',
-  'gender': 'Male',
-  'job': 'Student'
-
-}]
 class App extends Component {
+  
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+  
   render() {
     const {classes} = this.props;
     return (
@@ -65,7 +55,7 @@ class App extends Component {
         </TableHead>
         <TableBody>  
       {
-        customers.map(c => {
+        this.state.customers ? this.state.customers.map(c => {
           return (
             <Customer
             key={c.id}
@@ -77,7 +67,7 @@ class App extends Component {
             job={c.job}
             />
           )
-        })
+        }) : ""
       }
       </TableBody>
       </Table>
--- server.js
+++ server.js
@@ -6,8 +6,36 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: "hello express"});
+app.get('/api/customers', (req, res) => {
+    res.send([
+      {
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': 'Daniel Song',
+        'birthday': '910111',
+        'gender': 'Male',
+        'job': 'Student'
+      
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': 'Lionel Messi',
+        'birthday': '123456',
+        'gender': 'Male',
+        'job': 'Student'
+      
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': 'Good Job',
+        'birthday': '910111',
+        'gender': 'Male',
+        'job': 'Student'
+      
+      }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
